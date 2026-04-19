e2ecc70c7daa63a692c7ff74a38ce31b60aeaa2c
Alejandro Saenz <alejandrosaenz117@gmail.com>
2026-04-19 01:01:43 -0400

    chore: ignore tsc intermediate dist files, keep only ncc bundle

--- dist/sarif.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Finding } from './types.js';
-export declare function findingsToSarif(findings: Finding[]): any;
-export declare function sarifToBase64(sarif: any): string;
-//# sourceMappingURL=sarif.d.ts.map
